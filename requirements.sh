06e84a2b1356f849c81599edf7b118ae11be697f
lelee00 <66313687+lelee00@users.noreply.github.com>
2022-11-21 11:20:50 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get -y -qq --fix-missing install python3-pandas python3-numpy 
-sudo apt-get install python-pip3
+sudo apt-get install python3-pip
 pip3 install altair
 pip3 install vega_datasets
