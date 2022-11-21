eee454758159332e36fb157ba616056f49a3f40e
lelee00 <66313687+lelee00@users.noreply.github.com>
2022-11-21 14:47:20 -0500

    Update transform_visualize.py

--- transform_visualize.py
+++ transform_visualize.py
@@ -15,12 +15,11 @@ def create_interactive_chart():
   ansi = ansi[['id', 'abbr', 'LocationDesc']]
   
   print('transforming data...')
+  count_passed_bills = legislation[legislation.Status == 'Enacted'].groupby(
+    ['LocationDesc','GeoLocation'])[['ProvisionID']].drop_duplicates().agg('count').reset_index()
   
-  legislation_obesity = legislation.groupby(
-     ['LocationDesc','GeoLocation'])[['ProvisionID']].agg(
-    'count'
-  ).reset_index().merge(
-     activity[(activity.Question == 'Percent of adults aged 18 years and older who have obesity') 
+  legislation_obesity = count_passed_bills.merge(
+    activity[(activity.Question == 'Percent of adults aged 18 years and older who have obesity') 
               & (activity.StratificationCategoryId1 == 'OVR')].groupby(
       ['LocationDesc']
       ).Data_Value.agg(
@@ -29,13 +28,12 @@ def create_interactive_chart():
   ).merge(
       ansi
   ).rename(
-     columns = {'ProvisionID':'Number of Relevant Bills Proposed', 
+     columns = {'ProvisionID':'Number of Relevant Bills Enacted', 
                 'Data_Value':'Percent of Obese Adults','LocationDesc':'State'}
   )
 
   legislation_obesity['lat'] = legislation_obesity.GeoLocation.apply(lambda x: float(re.sub('[()]','',x).split(', ')[0]))
   legislation_obesity['lon'] = legislation_obesity.GeoLocation.apply(lambda x: float(re.sub('[()]','',x).split(', ')[1]))
-  print(legislation_obesity.head())
   
   print('visualizing data...')
   
@@ -45,7 +43,7 @@ def create_interactive_chart():
                       fields=['lat', 'lon'])
 
   base = alt.Chart(states).mark_geoshape(fill = 'lightgrey', stroke = 'white').properties(
-     title='Obesity in the United States versus Related Legislature Proposed, 2011 - 2017',
+     title='Obesity in the United States versus Related Legislature Enacted, 2011 - 2017',
      width=650,
      height=400
   ).project('albersUsa')
@@ -63,32 +61,41 @@ def create_interactive_chart():
      lookup = 'id', 
      from_=alt.LookupData(legislation_obesity, 
                           'id',
-                           ['Percent of Obese Adults',
-                           'State'])
+                           ['State', 'Percent of Obese Adults'])
   )
 
   points = alt.Chart(legislation_obesity).encode(
       longitude='lon:Q',
       latitude='lat:Q',
   ).mark_circle().encode(
-     color= alt.Color('Number of Relevant Bills Proposed:Q', 
+     color= alt.Color('Number of Relevant Bills Enacted:Q', 
                        scale = alt.Scale(scheme = 'redyellowgreen'), 
                        sort = 'ascending'
                      ),
      stroke = alt.value('white'),
      size=alt.condition(~hover, 
-                        'Number of Relevant Bills Proposed:Q', 
+                        'Number of Relevant Bills Enacted:Q', 
                         alt.value(1)
                         ),
-      tooltip = ['State',
-                 'Number of Relevant Bills Proposed',
-                'Percent of Obese Adults']
-  ).add_selection(
-     hover)
-
-
+      tooltip = ['State:N', 'Number of Relevant Bills Enacted:Q','Percent of Obese Adults:Q']
+  ).add_selection(hover)
+ 
+  scatter_points = alt.Chart(legislation_obesity).mark_point().encode(
+    alt.X('Number of Relevant Bills Enacted:Q'),
+    alt.Y('Percent of Obese Adults:Q')
+  )
+  scatter_line = alt.Chart(legislation_obesity).mark_line().encode(
+    alt.X('Number of Relevant Bills Enacted:Q'),
+    alt.Y('Percent of Obese Adults:Q')
+  )
+  
+ 
+  scattered = alt.layer(scatter_line, scatter_points)
   out = alt.layer(base, colors, points).resolve_scale(color = 'independent', size = 'independent')
+  
   out.save('us-obesity.html')
+  scattered.save('us-scatter.html')
+  
   
 if __name__ == '__main__':
 #   os.system('bash requirements.sh')
@@ -100,4 +107,5 @@ if __name__ == '__main__':
   os.system('gsutil cp CDC_nutrition-legislation.csv gs://' + bucket_name + '/nutrition/')
   os.system('gsutil cp CDC_nutrition-and-activity.csv gs://' + bucket_name + '/nutrition/')
   os.system('gsutil cp us-obesity.html gs://' + bucket_name + '/nutrition/')
+  os.system('gsutil cp us-scatter.html gs://' + bucket_name + '/nutrition/')
   
