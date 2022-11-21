ecbd109a74a29b441aa86f68659cdaee8b31fba8
lelee00 <66313687+lelee00@users.noreply.github.com>
2022-11-21 10:06:08 -0500

    Delete move_files.py

--- move_files.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-if __name__ == '__main__':
-  bucket_name = sys.argv[1]
-  os.system('bash cdc_data.sh')
-  os.system('gsutil cp CDC_nutrition-legislation.csv gs://' + bucket_name + '/nutrition/')
-  os.system('gsutil cp CDC_nutrition-and-activity.csv gs://' + bucket_name + '/nutrition/')
-  os.system('gsutil cp us-obesity.html gs://' + bucket_name + '/nutrition/')
