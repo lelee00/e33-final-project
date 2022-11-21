322c446b8e3c790d6dcc521675e88dae48a40eaf
lelee00 <66313687+lelee00@users.noreply.github.com>
2022-11-20 21:00:46 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,3 @@
 sudo apt-get update
-sudo apt-get -y -qq --fix-missing install python-altair python-numpy python-pandas python-vega_datasets python-re
+sudo apt-get install python3-pip
+pip -r requirements.txt
