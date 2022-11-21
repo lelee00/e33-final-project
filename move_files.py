4cd24807f23d582a8a13250075be07aef36345cc
lelee00 <66313687+lelee00@users.noreply.github.com>
2022-11-21 09:54:15 -0500

    Create move_files.py
    Moves data files and final visualization to user specified google cloud storage bucket

---
+++ move_files.py
@@ -0,0 +1,7 @@
+import os
+
+if __name__ == '__main__':
+  bucket_name = sys.argv[1]
+  os.system("gsutil cp CDC_nutrition-legislation.csv gs://' + bucket_name + '/nutrition/')
+  os.system("gsutil cp CDC_nutrition-and-activity.csv gs://' + bucket_name + '/nutrition/')
+  os.system("gsutil cp us-obesity.html gs://' + bucket_name + '/nutrition/')
